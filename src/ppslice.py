0edebfd0aa7536916bf1e926f4d901adf0e2991d
Ewan1165 <sumbyewan@gmail.com>
2022-12-03 15:53:51 +0000

    changed codebase

--- src/ppslice.py
+++ src/ppslice.py
@@ -1,21 +1,25 @@
-import json,tarfile,easygui,sys,os,shutil,subprocess,time,requests
-print("Select your portal 2 install directory, if an error message appears, press ok and ignore it")
-time.sleep(1.8)
-p2dir = easygui.diropenbox()
-packages = json.loads(requests.get("https://p2r3.com/spplice/packages",allow_redirects=True).content)
-iCount = 1
-print("0: Uninstall a mod")
-for package in packages["packages"]:
-    print(str(iCount) + ": " + package["title"])
-    iCount += 1
-modSelection = int(input("Select a mod: "))
-if modSelection == 0:
-    print("Uninstalled the mod!")
-    exit()
+import json,tarfile,os,subprocess,time,requests,shutil
+if "portal2.exe" in os.listdir():
+    shutil.rmtree("/portal2_tempcontent/")
+    packages = json.loads(requests.get("https://p2r3.com/spplice/packages",allow_redirects=True).content)
+    iCount = 1
+    print("0: Uninstall a mod")
+    for package in packages["packages"]:
+        print(str(iCount) + ": " + package["title"])
+        iCount += 1
+    modSelection = int(input("Select a mod: "))
+    if modSelection == 0:
+        print("Uninstalled the mod!")
+        exit()
+    else:
+        modURL = "https://p2r3.com/spplice/packages/" + packages["packages"][modSelection-1]["name"] + "/" + packages["packages"][modSelection-1]["file"]
+        serverFile = requests.get(modURL,allow_redirects=True)
+        open("ppslice_temp.tar","wb").write(serverFile.content)
+        file = tarfile.open("ppslice_temp.tar")
+        file.extractall(path="/portal2_tempcontent/")
+        file.close()
+        os.remove("ppslice_temp.tar")
+        subprocess.call("portal2.exe -novid -tempcontent")
 else:
-    modURL = "https://p2r3.com/spplice/packages/" + packages["packages"][modSelection-1]["name"] + "/" + packages["packages"][modSelection-1]["file"]
-    serverFile = requests.get(modURL,allow_redirects=True)
-    modArchive = open("ppslice_temp.tar","wb")
-    modArchive.write(serverFile.content)
-    extractedTar = tarfile.open("ppslice_temp.tar")
-    extractedTar.extractall(path=p2dir + "\portal2_dlc3")
+    print("Make sure this exe is in the same dir as your portal2.exe")
+    time.sleep(3)
