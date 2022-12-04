8610d982e9bca5d6884ee21cfae95085dbd9993b
Ewan1165 <sumbyewan@gmail.com>
2022-12-04 19:06:43 +0000

    Update ppslice.py

--- src/ppslice.py
+++ src/ppslice.py
@@ -1,6 +1,9 @@
 import json,tarfile,os,subprocess,time,requests,shutil
 if "portal2.exe" in os.listdir():
-    shutil.rmtree("/portal2_tempcontent/")
+    try:
+        shutil.rmtree(os.path.dirname(os.path.realpath(__file__)) + "\portal2_tempcontent")
+    except:
+        pass
     packages = json.loads(requests.get("https://p2r3.com/spplice/packages",allow_redirects=True).content)
     iCount = 1
     print("0: Uninstall a mod")
@@ -16,10 +19,9 @@ if "portal2.exe" in os.listdir():
         serverFile = requests.get(modURL,allow_redirects=True)
         open("ppslice_temp.tar","wb").write(serverFile.content)
         file = tarfile.open("ppslice_temp.tar")
-        file.extractall(path="/portal2_tempcontent/")
+        file.extractall(path=os.path.dirname(os.path.realpath(__file__)) + "\portal2_tempcontent")
         file.close()
         os.remove("ppslice_temp.tar")
-        subprocess.call("portal2.exe -novid -tempcontent")
+        subprocess.call("portal2.exe -novid -tempcontent +map intro1")
 else:
     print("Make sure this exe is in the same dir as your portal2.exe")
-    time.sleep(3)
